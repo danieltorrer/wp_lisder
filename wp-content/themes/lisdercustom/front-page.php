c57b283b0857944c32819a526d1896aaf7857d44
Daniel <daniel.torrer@gmail.com>
2013-10-02 18:23:58 -0500

    parallax

--- wp-content/themes/lisdercustom/front-page.php
+++ wp-content/themes/lisdercustom/front-page.php
@@ -3,18 +3,41 @@ get_header();
 ?>
 <div class="parallax-panel">
 	<ul id="scene">
-		<li class="layer" data-depth="0.05"><h1>Somos un laboratorio de investigación en energias renovables</h1></li>
+		<li class="layer img-scene" data-depth="0.9"><img src="<?php bloginfo('template_directory');?>/img/l1.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.8"><img src="<?php bloginfo('template_directory');?>/img/l4.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.4"><img src="<?php bloginfo('template_directory');?>/img/l5.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.5"><img src="<?php bloginfo('template_directory');?>/img/l6.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.6"><img src="<?php bloginfo('template_directory');?>/img/l7.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.6"><img src="<?php bloginfo('template_directory');?>/img/l8.png" alt=""></li>
+		<li class="layer img-scene" data-depth="0.3"><img src="<?php bloginfo('template_directory');?>/img/l9.png" alt=""></li>
+
+		<li class="layer" data-depth="0">
+			<h1>Somos un laboratorio de investigación en energias renovables</h1>
+		</li>
+
+		<li class="layer" data-depth="0">
+			<h3 class="subheader">Lorem ipsum dolor.</h3>
+		</li>
+
+		<li class="layer btn-scene" data-depth="0">
+			<div class="row">
+				<a href="#" class="button">Nosotros</a>
+			</div>
+		</li>		
 	</ul>
 </div>
 
 <div class="row">
-	<di class="large-12 columns">
+	<div class="large-12 columns">
 		<h1>Lorem i ad porro rerum hic.</h1>
 		<h4>Lorem ipsum algo</h4>
-
-
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, deserunt, maiores in illum ut dolore delectus molestias voluptatibus quod et quae quas asperiores quis id rerum reiciendis exercitationem magnam ipsam.</p>
-	</di>
+	</div>
+		
+	<div class="large-12 columns">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, numquam natus expedita rerum iste possimus ullam fugit maiores asperiores velit. Earum, ullam eveniet minus voluptatum ipsam soluta quae praesentium hic!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, aut, eius sed placeat excepturi maxime labore nobis nisi atque ipsam qui error veniam! Natus, libero dignissimos beatae. Sed, nam, beatae.</p>
+	</div>
 </div>
 
 <?php 
--- wp-content/themes/lisdercustom/header.php
+++ wp-content/themes/lisdercustom/header.php
@@ -10,12 +10,12 @@
 <!--<![endif]-->
 
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -25,12 +25,15 @@
 <body <?php body_class(); ?>>
 	<header id="" class="row" role="banner">
 		<hgroup class="large-4 columns">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+			<h1 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<img src="http://www.placehold.it/155x87&text=logo" alt="">
+				</a>
+			</h1>
+			</hgroup>
 
-		<nav id="" class="large-8 columns" role="navigation">
-			<?php 
+			<nav id="" class="large-8 columns" role="navigation">
+				<?php 
 				$defaults = array(
 					'container'       => 'false',
 					'menu_class'      => 'inline-list menu',
@@ -39,10 +42,10 @@
 					'after'           => '',
 					'link_before'     => '',
 					'link_after'      => '',
-				);
+					);
 				wp_nav_menu( $defaults ); 
-			?>
-		</nav>
-	</header>
+				?>
+			</nav>
+		</header>
 
-	<div id="main" class="wrapper">
+		<div id="main" class="wrapper">
