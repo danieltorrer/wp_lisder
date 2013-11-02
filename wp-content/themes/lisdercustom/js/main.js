2c44f9f65c85bcf0516e2903f00741c483073018
Daniel <daniel.torrer@gmail.com>
2013-11-02 15:06:16 -0600

    Filtering complete

--- wp-content/themes/lisdercustom/js/main.js
+++ wp-content/themes/lisdercustom/js/main.js
@@ -7,21 +7,23 @@ $(document).ready(function(){
 		timer: true
 	});
 
-
 	var $container = $('.isotope-container');
 
 	$container.isotope({
-
+		itemSelector : '.descripcion-articulo',
+		layoutMode : 'fitRows'
 	});
 
 	$(".publicacion-tax .wp-tag-cloud li a").attr("href","#")
 
 	$(".publicacion-tax .wp-tag-cloud li a").click(function(event){
 		event.preventDefault();
-		$(this).attr("data-filter", $(this).text().replace(" ","_"))
-		var selector = $(this).attr('data-filter');
-		$container.isotope({ filter: selector });
+		
+		//$(this).attr("data-filter")
+		//var selector = $(this).attr('data-filter');
+		console.log("." + $(this).text().replace(" ","_"))
+		
+		$container.isotope({ filter: "." + $(this).text().replace(" ","_") });
 		return false;
-
 	})
 })
--- wp-content/themes/lisdercustom/publicaciones.php
+++ wp-content/themes/lisdercustom/publicaciones.php
@@ -34,46 +34,18 @@ $the_query = new WP_Query( $args);
 		
 		<?php if( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); 
 		?>
-		<div class="row">
-			<!--<div class='large-12 columns descripcion-articulo' data-filter='<?php
 
-			$terms = get_terms("fecha");
-			$count = count($terms);
-			if ( $count > 0 ){
-
-				foreach ( $terms as $term ) {
-					echo str_replace(" ", "_", $term->name);
-				}
-
-			}
-			?> <?php
-
-				$terms = get_terms("revista");
-				$count = count($terms);
-				if ( $count > 0 ){
-
-					foreach ( $terms as $term ) {
-						echo str_replace(" ", "_", $term->name);
-					}
-
-				}
-				?>'>-->
-
-				<div class="large-12 columns descripcion-articulo" data-filter='<?php echo simple_fecha();?> <? echo simple_revista(); ?> '>
-
-
-					<h3> <?php the_title()?> </h3>
-					<?php the_content() ?>
-					<!--<p class="subheader"><?php echo get("autores") ?></p>-->
-					<small>Año: <cite><?php echo simple_fecha();?></cite></small>
-					<br>
-					<small>Revista: <cite><? echo simple_revista(); ?></cite></small>
+		<div class="large-12 columns descripcion-articulo <?php echo simple_fecha();?> <? echo str_replace(" ", "_", simple_revista()); ?>" >
 			
-			</div>
-
+			<h3> <?php the_title()?> </h3>
+			<?php the_content() ?>
+			<!--<p class="subheader"><?php echo get("autores") ?></p>-->
+			<small>Año: <cite><?php echo simple_fecha();?></cite></small>
+			<br>
+			<small>Revista: <cite><? echo simple_revista(); ?></cite></small>
+			<hr>
 		</div>
-		<hr>
-
+		
 		<?php endwhile; else: 
 		?>
 		<?php endif; 
